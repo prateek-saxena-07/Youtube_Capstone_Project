0026330656ad88472ddbcf50d726c98f8af08c51
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-10-14 11:19:55 +0530

    patching commit

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,6 +1,6 @@
 import User from "../model/user.model.js";
 import Video from "../model/video.model.js";
-
+import { createError } from "../error.js";
 
 //Get particular user to upadate user's like and dislikes
 
@@ -12,7 +12,28 @@ export const getUser = async (req, res) => {
 
   res.status(200).json(others);
 }
+export const update = async (req, res, next) => {
+  //req.user.id comes from verifytoken middleware
 
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      const { password, ...others } = updatedUser._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      next(err);
+      //goes to custom error handling middleware in server.js
+    }
+  } else {
+    return next(createError(403, "You can only update your account"));
+  }
+};
 
 export const like = async (req, res, next) => {
   const id = req.user.id;
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { like,dislike,getUser } from '../controller/user.js';
+import {update,like,dislike,getUser } from '../controller/user.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 //like a video
 router.put("/like/:videoId", verifyToken, like);
 
+router.patch("/:id", verifyToken, update);
 //dislike a video
 router.put("/dislike/:videoId", verifyToken, dislike);
 
